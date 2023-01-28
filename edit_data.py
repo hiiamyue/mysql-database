298df9166da5961013ff655cec704fb6e2338171
hiiamyue <nabumafu@gmail.com>
2023-01-28 12:14:57 +0000

    modified data + add index

--- edit_data.py
+++ edit_data.py
@@ -1,19 +1,42 @@
 import csv
 import re
 
-def fix_genres(source, dest):
-    with open(dest, 'w', newline='') as file:
-        writer = csv.writer(file)
+def create_genres(source,dest):
+    with open(dest,'w',newline='') as file:
+        writer =csv.writer(file)
         with open(source) as f:
-            reader = csv.reader(f, delimiter=',')
+            reader = csv.reader(f,delimiter=',')
             for r in reader:
-                genres = r[1].split('|')
+                genres = r[2].split('|')
                 for genre in genres:
                     writer.writerow([r[0], genre])
 
 
+def add_index(source,dest):
+     with open(dest,'w',newline='') as file:
+        writer =csv.writer(file)
+        with open(source) as f:
+            reader = csv.reader(f,delimiter=',')
+            row1 = next(reader)
+            row1.append('index')
+            writer.writerow(row1)
+            index =0
+            for r in reader:
+                writer.writerow(r+[index])
+                index +=1
+
+# def fix_genres(source, dest):
+#     with open(dest, 'w', newline='') as file:
+#         writer = csv.writer(file)
+#         with open(source) as f:
+#             reader = csv.reader(f, delimiter=',')
+#             for r in reader:
+#                 genres = r[1].split('|')
+#                 for genre in genres:
+#                     writer.writerow([r[0], genre])
+
+
 def modified_movies(source,dest):
-    list =[]
     with open(source) as f:
         with open(dest,'w',newline='') as d:
             writer = csv.writer(d)
@@ -40,10 +63,12 @@ def modified_movies(source,dest):
                         writer.writerow(l+[0])
                         # null value represented as 0
                     
-    
-    
 
 if __name__=="__main__":
-    fix_genres('Normalised/genres.csv','Normalised/new_genres.csv' )
-    list =modified_movies('Normalised/movies.csv','Normalised/new_movies.csv')
+    create_genres('ml-latest-small/movies.csv','Normalised/genres.csv')
+    add_index('Normalised/genres.csv','Normalised/new_genres.csv')
+    add_index('ml-latest-small/tags.csv','Normalised/new_tags.csv')
+    #fix_genres('Normalised/genres.csv','Normalised/new_genres.csv' )
+    modified_movies('ml-latest-small/movies.csv','Normalised/new_movies.csv')
+
  
