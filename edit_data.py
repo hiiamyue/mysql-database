49f43686fd8338e1b49eeb961ed3ddb45ecc4995
hiiamyue <nabumafu@gmail.com>
2023-01-28 14:28:38 +0000

    fixed shit

--- edit_data.py
+++ edit_data.py
@@ -18,7 +18,7 @@ def add_index(source,dest):
         with open(source) as f:
             reader = csv.reader(f,delimiter=',')
             row1 = next(reader)
-            row1.append('index')
+            row1.append('idx')
             writer.writerow(row1)
             index =0
             for r in reader:
@@ -36,12 +36,14 @@ def add_index(source,dest):
 #                     writer.writerow([r[0], genre])
 
 
+
+
 def modified_movies(source,dest):
     with open(source) as f:
         with open(dest,'w',newline='') as d:
             writer = csv.writer(d)
             reader = csv.reader(f, delimiter=',')
-            row1 = next(reader)#dont need first line
+            row1 = next(reader)[:2]#dont need first line
             row1.append('release_date')
             writer.writerow(row1)
             for l in reader:
@@ -50,25 +52,35 @@ def modified_movies(source,dest):
                     d =date[-1].strip("(,)")
                     new=l[1].replace(date[-1],'').rstrip().strip('"')
                     l[1]=new
-                    writer.writerow(l+[d])
+                    writer.writerow(l[:2]+[d])
                 else:
                     if date:
                         d = date[0].strip("(,)")
                         new =l[1].replace(date[0],'').rstrip().strip('"')
                         l[1]=new
-                        writer.writerow(l+[d])
+                        writer.writerow(l[:2]+[d])
                     else:
                         new=l[1].strip('"')
-                        l[1]=[new]
-                        writer.writerow(l+[0])
+                        l[1]=new
+                        writer.writerow(l[:2]+[0])
                         # null value represented as 0
-                    
+def new_movies(source1,source2,dest):
+     with open(dest, 'w', newline='') as file:
+        writer = csv.writer(file)
+        with open(source1) as f,open(source2)as mv:
+            reader1 = csv.reader(f, delimiter=',')
+            reader2 = csv.reader(mv,delimiter=',')
+            for l , l2 in zip(reader1,reader2):
+                writer.writerow(l2+l[1:])
+                
 
 if __name__=="__main__":
+    #new_movies()
     create_genres('ml-latest-small/movies.csv','Normalised/genres.csv')
     add_index('Normalised/genres.csv','Normalised/new_genres.csv')
     add_index('ml-latest-small/tags.csv','Normalised/new_tags.csv')
     #fix_genres('Normalised/genres.csv','Normalised/new_genres.csv' )
-    modified_movies('ml-latest-small/movies.csv','Normalised/new_movies.csv')
+    modified_movies('ml-latest-small/movies.csv','Normalised/movies.csv')
+    new_movies('ml-latest-small/links.csv','Normalised/movies.csv','Normalised/new_movies.csv')
 
  
