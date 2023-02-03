56ff48181353cbda7bf7acc6f325dcd86fd83de0
Vincent Lefeuve <vincentlfve@gmail.com>
2023-02-03 23:59:52 +0000

    Easier for json to list of objects

--- python/src/controller/controller.py
+++ python/src/controller/controller.py
@@ -7,39 +7,17 @@ class Controller:
 
     def get_default_data(self):
         data= self.model.get_default_data()
-        json_data=[]
-        for movie in data:
-            json_obj ={"movieId":movie[0], "title":movie[1],"release_date":movie[2],\
-                       "imdbId":movie[3],"tmdbId":movie[4]}
-            json_data.append(json_obj)
+        json_data = json.dumps(data)
         return json_data
     
     def sort_by_date(self):
         data= self.model.sort_by_date()
-        json_data=[]
-        for movie in data:
-            json_obj ={"movieId":movie[0], "title":movie[1],"release_date":movie[2],\
-                       "imdbId":movie[3],"tmdbId":movie[4]}
-            json_data.append(json_obj)
+        json_data = json.dumps(data)
         return json_data
     
     def sort_by_title(self):
         data= self.model.sort_by_title()
-        json_data=[]
-        for movie in data:
-            json_obj ={"movieId":movie[0], "title":movie[1],"release_date":movie[2],\
-                       "imdbId":movie[3],"tmdbId":movie[4]}
-            json_data.append(json_obj)
+        json_data = json.dumps(data)
         return json_data
-    
-    def sort_by_rating(self):
-        data= self.model.sort_by_rating()
-        json_data=[]
-        for movie in data:
-            json_obj ={"movieId":movie[0], "title":movie[1],"release_date":movie[2],\
-                       "imdbId":movie[3],"tmdbId":movie[4]}
-            json_data.append(json_obj)
-        return json_data
-    
     
     
