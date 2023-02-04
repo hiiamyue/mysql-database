cd0e07953f57c0f4c6c6c4dc994c588738c4aaf7
hiiamyue <nabumafu@gmail.com>
2023-02-04 08:19:35 +0800

    Do not close cursor after each function call

--- python/src/model/model.py
+++ python/src/model/model.py
@@ -22,26 +22,30 @@ class Model:
     def get_default_data(self):
         self.cursor.execute("SELECT * FROM movies")
         movies = self.cursor.fetchall()
-        self.cursor.close()
+        #self.cursor.close()
         return movies
 
     def sort_by_date(self):
         self.cursor.execute("SELECT * FROM movies \n ORDER BY release_date")
         movies = self.cursor.fetchall()
-        self.cursor.close()
+        #self.cursor.close()
         return movies
     
     def sort_by_title(self):
         self.cursor.execute("SELECT * FROM movies \n ORDER BY title")
         movies = self.cursor.fetchall()
-        self.cursor.close()
+        #self.cursor.close()
         return movies
+    
+    def close_cursor(self):
+        self.cursor.close()
     
 #     SELECT C.id_car, name, AVG(rating_value) AS average
 # FROM car C JOIN rating R 
 #       ON C.id_car = R.id_car
 # GROUP By C.id_car, name
 # ORDER BY average DESC
+
     def sort_by_rating(self):
         self.cursor.execute("""     SELECT
                                 \n  m.*, AVG(r.rating) AS average
@@ -50,6 +54,6 @@ class Model:
                                 \n  GROUP BY m.movie_id
                                 \n  ORDER BY average DESC""")
         movies = self.cursor.fetchall()
-        self.cursor.close()
+        #self.cursor.close()
         return movies
     
