d0c9b9172248fd23b3fb04c5298a9a43fae33907
hiiamyue <nabumafu@gmail.com>
2023-02-15 19:59:00 +0800

    fixed limit sytax

--- python/src/model/model.py
+++ python/src/model/model.py
@@ -19,16 +19,6 @@ class Model:
         self.db = mydb
         self.cursor = self.db.cursor(dictionary=True)
 
-
-    def create_average_table(self):
-        query = """CREATE TABLE average_rating(
-                SELECT movie_id,AVG(rating) as rating
-                FROM ratings
-                GROUP BY movie_id
-                ORDER BY rating DESC
-                    )"""
-        self.cursor.execute(query)
-        
     
     def get_genre_type(self):
         self.cursor.execute('SELECT DISTINCT genre FROM genres')
@@ -36,13 +26,15 @@ class Model:
         return distinct_genre
     
 
-    def sorting(self, sort_by_date, sort_by_title, sort_by_rating, query):
+    def sorting(self, sort_by_date, sort_by_title, sort_by_rating,offset, query):
         if sort_by_rating:
             query = query + 'ORDER BY r.rating DESC'
         elif sort_by_title:
             query = query + 'ORDER BY m.title ASC'
         elif sort_by_date:
             query = query + 'ORDER BY m.release_date ASC'
+        if offset:
+            query = query+'LIMIT 23 OFFSET {}'.format(offset)
 
         return query
     
@@ -73,11 +65,9 @@ class Model:
 
         return select_genre, select_date, select_rating, offset
     
-
-    def get_film_by_genre_date_rating(self, genre, date_start, date_end, rating_min, rating_max, sort_by_date, sort_by_title, sort_by_rating, page):
+    def get_film_by_genre_date_rating(self,genre,date_start,date_end,rating_min,rating_max,sort_by_date,sort_by_title,sort_by_rating,page):
+        selected_genre,selected_date,selected_rating,offset =self.gen_query_for_view(genre,date_start,date_end,rating_min,rating_max,page)
         
-        selected_genre, selected_date, selected_rating, offset = self.gen_query_for_view(genre, date_start, date_end, rating_min, rating_max, page)
-       
         query = ("""SELECT  DISTINCT m.*,CONVERT(r.rating, CHAR) AS rating 
                     \n FROM movies m INNER JOIN genres g
                     \n on m.movie_id = g.movie_id
@@ -86,26 +76,22 @@ class Model:
                     \n INNER JOIN average_rating r 
                     \n on m.movie_id = r.movie_id
                     {2}
-                    LIMIT 23
-                    OFFSET {3}
-                    \n""".format(selected_date, selected_genre, selected_rating, offset))
+                    \n""".format(selected_date, selected_genre, selected_rating))
         
-        query_after_sorting = self.sorting(sort_by_date, sort_by_title, sort_by_rating, query)
+        query_after_sorting = self.sorting(sort_by_date, sort_by_title, sort_by_rating,offset, query)
         self.cursor.execute(query_after_sorting)
         
         movies = self.cursor.fetchall()
         return movies
     
-
-    def get_page_data(self, page):
-        query = ("""SELECT * FROM movies
-                ORDER BY movie_id
-                LIMIT 23
-                OFFSET {}""").format((page - 1) * 23)
+    def create_average_table(self):
+        query = """CREATE TABLE average_rating(
+                SELECT movie_id,AVG(rating) as rating
+                FROM ratings
+                GROUP BY movie_id
+                ORDER BY rating DESC
+                    )"""
         self.cursor.execute(query)
-        movies = self.cursor.fetchall()
-        return movies
     
-
     def close_cursor(self):
         self.cursor.close()
