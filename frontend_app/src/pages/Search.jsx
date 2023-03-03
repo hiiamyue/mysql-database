4177610b16cb0683c5ecbd4466ab3db17b68743a
Vincent Lefeuve <vincentlfve@gmail.com>
2023-03-03 11:01:27 +0000

    Finished implementing search frontend

--- frontend_app/src/pages/Search.jsx
+++ frontend_app/src/pages/Search.jsx
@@ -4,34 +4,27 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import MovieCard from "../components/MovieCard";
 import LoadingMovieCard from "../components/LoadingMovieCard"
-import { getPage, pageParamsToAPIParams } from '../utils/QueryUtils';
+import Skeleton from '@mui/material/Skeleton';
 
-// TODO: change to dark mode
+
 const Search = () => {
     const [searchParams, setSearchParams] = useSearchParams();
     const [movies, setMovies] = useState([]);
     const [hasFetched, setFetched] = useState(false);
-    let maxPage = 100;
     
-    function onPageChange(page){
-        searchParams.set("page", page)
-        setSearchParams(searchParams)
-    }
-
     useEffect(() => {
 
-        const url = `http://localhost:8000/movies${pageParamsToAPIParams(searchParams)}`;
+        const url = `http://localhost:8000/search?query=${searchParams.get("query")}`;
         try {
-            
+
+        setFetched(false)  
         console.log(url)
         
         fetch(url)
         .then((res) => res.json())
         .then((data) => {
             console.log("--render--")
-           // setFetched(false)
-           // maxPage = data["pagination"]['max_page']
-           setMovies(data["results"]);
+           setMovies(data);
            setFetched(true)
         })
         }
@@ -49,15 +42,15 @@ const Search = () => {
                 
                 <div className='max-w-[1240px] mx-auto text-white mt-4'>
                     <h1 className='md:text-6xl sm:text-6xl text-4xl font-bold md:py-6 sm:px-6 '>Search results for {searchParams.get('query')} </h1>
-                    <p className=' ml-6  md:text-2xl sm:text-xl text-slate-600'>Found {movies.length} movies.</p>
+                    <p className=' ml-6  md:text-2xl sm:text-xl text-slate-600'>Found {hasFetched ? movies.length : <Skeleton variant="circular" animation="wave" width={210} height={118} />} movies.</p>
 
                 </div >
-                <div className='max-w-[1240px] mx-auto mb-40'>
+                <div className='max-w-[1240px] mx-auto min-h-screen'>
                     {
                         hasFetched ?
-                        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6  xl:grid-cols-7 gap-4 mt-20 px-0">
+                        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4  xl:grid-cols-5 gap-4 mt-20 px-0">
                             {movies.map((movie) => (
-                                    <MovieCard title={movie.title} release_date={movie.release_date} rating={movie.avg_rating}/>
+                                    <MovieCard title={movie.title} release_date={movie.release_date} rating={12}/>
                             ))} 
                         </div>
                         :
