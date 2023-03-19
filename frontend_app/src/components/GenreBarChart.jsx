c6fda4772efd78878140298466acdb091760bf3c
Vincent Lefeuve <vincentlfve@gmail.com>
2023-03-19 00:55:30 +0000

    Frontend for task 3

---
+++ frontend_app/src/components/GenreBarChart.jsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+import { Column } from '@ant-design/plots';
+
+const GenreBarChart = ({movieid, group}) => {
+  const [ratings, setRatings] = useState([])
+  const [fetchedRatings, setFetchedRatings] = useState(false)
+  const [field, setField] = useState("")
+  useEffect(() => {
+    if (group === 'high'){
+      setField("high_raters_avg")
+    } else {
+      setField("low_raters_avg")
+    }
+    asyncFetch();
+    setFetchedRatings(true)
+  }, []);
+
+  const asyncFetch = () => {
+    
+    fetch(`http://localhost:8000/q3_2_reaction_genre?movie_id=${movieid}&group=${group}`)
+      .then((response) => response.json())
+      .then((json) => {
+        setRatings(json)
+        config["data"] = json
+        console.log(ratings)
+      })
+      .catch((error) => {
+        console.log('fetch data failed', error);
+      });
+  };
+
+  const config = {
+    data: ratings,
+    xField: 'genre',
+    yField: field,
+    color: '#e879f9',
+    label: {
+      
+      position: 'middle',
+      
+      style: {
+        fill: '#FFFFFF',
+        opacity: 0.6,
+      },
+    },
+    xAxis: {
+      label: {
+        autoHide: true,
+        autoRotate: false,
+      },
+    },
+    meta: {
+      type: {
+        alias: 'x',
+      },
+      sales: {
+        alias: 'y',
+      },
+    },
+  };
+  return (
+    <div className='pt-10 md:mx-0 mx-8'>
+        {
+        fetchedRatings ?
+        <Column {...config} />
+        :
+        <div>Fetching ratings</div>
+        }
+    </div>
+  );
+};
+
+export default GenreBarChart;
--- frontend_app/src/components/GenreProfileRadar.jsx
+++ frontend_app/src/components/GenreProfileRadar.jsx
@@ -1,49 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
 import { Radar } from '@ant-design/plots';
 import Skeleton from '@mui/material/Skeleton';
 
-const GenreProfileRadar = () => {
+const GenreProfileRadar = ({genre, like}) => {
   const [hasFetchedProfile, setFetchedProfile] = useState(false)
-  const data = [
-    {
-      name: 'G2',
-      star: 10371,
-    },
-    {
-      name: 'G6',
-      star: 7380,
-    },
-    {
-      name: 'F2',
-      star: 7414,
-    },
-    {
-      name: 'L7',
-      star: 2140,
-    },
-    {
-      name: 'X6',
-      star: 660,
-    },
-    {
-      name: 'AVA',
-      star: 885,
-    },
-    {
-      name: 'G2Plot',
-      star: 1626,
-    },
-  ];
+  const [profile, setProfile] = useState([])
+
+  useEffect(() => {
+    asyncFetch();
+  }, []);
+
+  const asyncFetch = () => {
+    fetch(`http://localhost:8000/q6?genre=${genre}&type=${like}`)
+      .then((response) => response.json())
+      .then((json) => {
+        setProfile(json)
+        config["data"] = json
+        console.log(json)
+        setFetchedProfile(true)
+      })
+      .catch((error) => {
+        console.log('fetch data failed', error);
+      });
+  };
+
+  
+  
   const config = {
-    data: data.map((d) => ({ ...d, star: Math.sqrt(d.star) })),
-    xField: 'name',
-    yField: 'star',
+    data: profile,
+    xField: 'trait',
+    yField: 'stat',
     width: 1240,
     appendPadding: [0, 10, 0, 10],
     meta: {
       star: {
-        alias: 'star 数量',
+        alias: 'Personnality trait',
         min: 0,
         nice: true,
         formatter: (v) => Number(v).toFixed(2),
@@ -54,6 +45,8 @@ const GenreProfileRadar = () => {
     },
     yAxis: {
       label: false,
+      min: 0,
+      max: 7,
       grid: {
         alternateColor: 'rgba(0, 0, 0, 0.04)',
       },
@@ -65,16 +58,16 @@ const GenreProfileRadar = () => {
     area: {},
   };
   return (
-    <div className='mt-20 mx-0'>
-      <a  class="block max-w-sm sm:max-w-screen-2xl p-6  m-6 xl:m-20 mx-auto sm:mx-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        <h5 class="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">Average Comedy enjoyer</h5>
+    <div className='mt-20 mx-auto'>
+      <a  class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Average Comedy {like === "high" ? "enjoyer" : "disliker"}</h5>
         <p class="font-normal text-gray-700 dark:text-gray-400">Psychological profile.</p>
         {
           hasFetchedProfile ?
           <Radar {...config} />
           
           :
-          <Skeleton variant="rectangular" animation="wave" width={1142} height={600} className='mt-6 px-6 max-w-sm sm:max-w-[1032px]' />
+          <Skeleton variant="rectangular" animation="wave" width={1142} height={600} className='mt-6 px-6 max-w-[21rem]  max-h-[21rem] ' />
           
         }
       </a>
